91d1857f32711ae0c3ebd9b85adff0e522889d0c
P4ul0Silva <paulors43@outlook.com>
2023-02-24 22:00:28 -0300

    initial commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Paulo Silva",
+  githubUser: "P4ul0Silva",
+  linkedinUser: "paulors43",
+  whatsappNumber: "+55991951602",
+  emailUser: "paulors43@outlook.com",
+  instagramUser: "paulors43",
+  facebookUser: "paulors43",
+};
